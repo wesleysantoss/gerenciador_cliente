af5374d72767612b2f96154003078da3d0911602
Wesley <wessleysanttos@live.com>
2019-08-13 21:50:33 -0300

    Finalizado a view de login

--- app/views/login.php
+++ app/views/login.php
@@ -1,5 +1,16 @@
 <div class="container">
-    <div class="col-12">
-        <h4 class="text-center mt-2">Home</h4>
+    <div class="d-flex flex-row-reverse">
+        <div class="col-12 col-md-6 mt-5 text-center">
+            <div class="jumbotron">
+                <h1 class="display-4">Login</h1>
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Usuário">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Senha">
+                </div>
+                <button type="submit" class="btn btn-success btn-block">Entrar</button>
+            </div>
+        </div>
     </div>
 </div>
